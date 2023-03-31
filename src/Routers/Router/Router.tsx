ca116fde6dceb85bb348c1289e9a93b51c80d7d1
jibananando <jibananando.pramanik@gmail.com>
2023-04-01 04:05:35 +0600

    complated navbar

--- src/Routers/Router/Router.tsx
+++ src/Routers/Router/Router.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
+import About from "../../Pages/Home/About/About";
+import Brands from "../../Pages/Home/Brands/Brands";
+import Hero from "../../Pages/Home/Hero/Hero";
 import Home from "../../Pages/Home/Home/Home";
+import Package from "../../Pages/Home/Package/Package";
+import Services from "../../Pages/Home/Services/Services";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +15,26 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/brands",
+        element: <Brands />,
+      },
+      {
+        path: "/hero",
+        element: <Hero />,
+      },
+      {
+        path: "/packages",
+        element: <Package />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
       },
     ],
   },
