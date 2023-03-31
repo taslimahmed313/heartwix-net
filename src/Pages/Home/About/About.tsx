6d49a8867578d6994e706e79bf05c1c8989ddfeb
Aminulhoque01 <aminuhoque53@gmail.com>
2023-03-31 14:23:10 +0600

    about commit

--- src/Pages/Home/About/About.tsx
+++ src/Pages/Home/About/About.tsx
@@ -1,7 +1,16 @@
 const About = () => {
   return (
     <div>
-      <h1>This is About Section</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img width='350' src="http://amberit.com.bd/img/about.png" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
