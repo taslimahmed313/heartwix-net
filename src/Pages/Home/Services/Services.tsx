e41d216b10f9915f021de8f7657250635c9e1433
Taslim <taslimahmed313@gmil.com>
2023-04-01 17:47:30 +0600

    Service Some changes

--- src/Pages/Home/Services/Services.tsx
+++ src/Pages/Home/Services/Services.tsx
@@ -1,7 +1,48 @@
 const Services = () => {
+  const serviceItems = [
+    {
+      serviceName: "Corporate Internet",
+      description:
+        "We offer safe internet access services with various service level descriptions for corporate businesses and SMEs.",
+      icon: "MdManageAccounts",
+    },
+    {
+      serviceName: "Corporate Internet",
+      description:
+        "We offer safe internet access services with various service level descriptions for corporate businesses and SMEs.",
+      icon: "MdManageAccounts",
+    },
+    {
+      serviceName: "Corporate Internet",
+      description:
+        "We offer safe internet access services with various service level descriptions for corporate businesses and SMEs.",
+      icon: "MdManageAccounts",
+    },
+    {
+      serviceName: "Corporate Internet",
+      description:
+        "We offer safe internet access services with various service level descriptions for corporate businesses and SMEs.",
+      icon: "MdManageAccounts",
+    },
+  ];
+
   return (
     <div>
-      <h1>Here Stay Services Section</h1>
+      <div>
+        <p>Your one stop smart internet solution !</p>
+        <h3>What We Do ?</h3>
+        <h4>Super Fast & Reliable Faster Network</h4>
+        <p>
+          Amber IT provides an extensive range of high quality data & internet
+          connectivity services throughout the country. We offer safe internet
+          access services with various service level descriptions for corporate
+          businesses and SMEs. Our IPTSP services allow the opportunity to
+          generate profitable and recurring monthly revenue for businesses. We
+          also offer hosting & web development solutions for any business with
+          high availability and consistency.
+        </p>
+      </div>
+      <div></div>
     </div>
   );
 };
